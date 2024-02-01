7d2b7368a32ea640c563aaae6530e8aa4b426783
sa3ina <sravnennaya@mail.ru>
2024-02-01 22:36:02 +0400

    hero and about section

--- Frontend/src/assets/components/AboutSection.tsx
+++ Frontend/src/assets/components/AboutSection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Grid from "@mui/material/Grid";
 type Props = {};
 
 const AboutSection = (props: Props) => {
@@ -21,6 +21,51 @@ const AboutSection = (props: Props) => {
         </p>
         <button>Know about us</button>
       </div>
+
+      <Grid container className="grid">
+        <Grid item lg={3} md={6} sm={6} xs={12} className="griditem">
+          <img
+            src="https://assets-global.website-files.com/63b2816edd90444c9df54d80/63ddf2c16af00b6ad3362796_icon-01.svg"
+            alt=""
+          />
+          <p className="top">Apply to humans</p>
+          <p className="bottom">
+            Handsome met debating sir dwelling age material. As style lived he
+            worse dried.
+          </p>
+        </Grid>
+        <Grid item lg={3} md={6} sm={6} xs={12} className="griditem">
+          <img
+            src="https://assets-global.website-files.com/63b2816edd90444c9df54d80/63ddf2dae4e25f67abe9a15e_icon-02.svg"
+            alt=""
+          />
+          <p className="top">Fast and easy</p>
+          <p className="bottom">
+            Speedily say has suitable disposal add boy. On fourth doubt miles of
+            child.
+          </p>
+        </Grid>
+        <Grid item lg={3} md={6} sm={6} xs={12} className="griditem">
+          <img
+            src="https://assets-global.website-files.com/63b2816edd90444c9df54d80/63ddf2e7dd106655ba11e466_icon-03.svg"
+            alt=""
+          />
+          <p className="top">Instantly stand out</p>
+          <p className="bottom">
+            Prosperous understood Middletons in conviction an uncommonly do
+          </p>
+        </Grid>
+        <Grid item lg={3} md={6} sm={6} xs={12} className="griditem">
+          <img
+            src="https://assets-global.website-files.com/63b2816edd90444c9df54d80/63ddf2f211b8731683906c29_icon-04.svg"
+            alt=""
+          />
+          <p className="top">Real time feedback</p>
+          <p className="bottom">
+            Supposing so be resolving breakfast am or perfectly months lasted.
+          </p>
+        </Grid>
+      </Grid>
     </div>
   );
 };
